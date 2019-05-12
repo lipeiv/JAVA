1f21f8b297c49d0a918df689a1df99da1be88cb5
lipeiv <lipeiv@qq.com>
2019-05-12 23:31:48 +0800

    java

--- He/src/com/company/Main.java
+++ He/src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
-public class Main {
-
-    public static void main(String[] args) {
-	System.out.println("Hello Java");
+abstract class GetTime {
+    public void getTime() {
+        long start = System.currentTimeMillis();
+        runcode();
+        long end = System.currentTimeMillis();
+        System.out.println("毫秒:"+(end-start));
     }
+    public abstract void runcode();
+}
+
+class Subtime extends GetTime {
+    public void runcode()
+    {
+        for (int x = 0; x<4000; x++)
+        {
+            System.out.print(x);
+        }
+        System.out.println("\n");
+    }
+}
+
+class Main{
+    public static void main(String[] args)
+    {
+        Subtime tm = new Subtime();
+        tm.getTime();
+    }
+
 }
