0abfc29ea65272b75253402758369bc7c3f92f98
lipeiv <lipeiv@qq.com>
2019-05-20 20:56:41 +0800

    java

---
+++ CollectionDemo.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class CollectionDemo
+{
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList();
+		//添加对象；
+		al.add("java01");
+		al.add("java02");
+
+		sop(al);
+	}
+
+	public static void sop(Object obj){
+		System.out.println(obj);
+	}
+
+}
